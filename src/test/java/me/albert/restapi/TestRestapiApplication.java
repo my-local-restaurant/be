ab29c41603546a6d16c428d8dc9a31e696d87b7d
ALbertIM0427 <guang7226961@naver.com>
2024-11-26 11:55:17 +0900

    chore: 프로젝트 초기화

---
+++ src/test/java/me/albert/restapi/TestRestapiApplication.java
@@ -0,0 +1,11 @@
+package me.albert.restapi;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(RestapiApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
